a9ad9084a2bef942bf569cd5a698301aaaa2bb36
Joseph <joseph.dillon.522@gmail.com>
2013-08-15 14:05:54 -0700

    Created mockup

--- public/javascripts/index.js
+++
@@ -1,5 +0,0 @@
-$(document).on('submit', "#form", function() {
-	req.session.username = $("#name").val();
-	console.log("submit event");
-	socket.emit('set_user', { user: req.session.username });
-});
