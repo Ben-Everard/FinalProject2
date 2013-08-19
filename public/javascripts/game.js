dd6b821f66c5a71381c91750831b9cb89cac3208
Joseph <joseph.dillon.522@gmail.com>
2013-08-19 12:05:17 -0700

    Chat works.

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,60 +2,63 @@
 var gameRef = new Firebase('https://finalprojectcd.firebaseio.com/game');
 var player1Ref = new Firebase('https://finalprojectcd.firebaseio.com/game/players/player1')
 var player2Ref = new Firebase('https://finalprojectcd.firebaseio.com/game/players/player2')
+var potRef = new Firebase('https://finalprojectcd.firebaseio.com/game/pot');
+var chatRef = new Firebase('https://finalprojectcd.firebaseio.com/game/chat');
 
+potRef.set({total:0});
 player1Ref.set({ name:'', chips:10000, card1:'', card2:'', hand:'', inPlay: true});
 player2Ref.set({ name:'', chips:10000, card1:'', card2:'', hand:'', inPlay: true});
 
-
-
+// Keeps track of everyone's chip count
 player1Ref.on('value', function (snapshot){
   var chips = snapshot.val().chips;
-  var name = snapshot.val().name;
-  $('#player1name').html(name);
+  console.log(chips);
   $('#player1chips').html(chips);
 })
 
-// console.log(player1Ref);
+player2Ref.on('value', function (snapshot){
+  var chips = snapshot.val().chips;
+  console.log(chips);
+  $('#player2chips').html(chips);
+})
+
+// Keeps track of the amount in the pot
+potRef.on('value', function (snapshot){
+  var pot = snapshot.val().total;
+  console.log(pot);
+  $('#pot').html(pot);
+})
 
 $(document).ready(function(){
-  
   //fire join game modal at start
   $('#joinGame').foundation('reveal', 'open'); 
-  
-  // add a player to the DB
-  // $('#join').click(function() {
-  //   var name = $('#username').val();
-  //   var playerName = $('#username').val();
-  //   playerName = playerRef.child(name).set({ name:name, chips:10000, card1:'', card2:'', hand:'', inPlay: true});
-  //   // console.log(playerName); 
-  // });
-
-   
-  // playerRef.on('value', function (snapshot) {
-  //   var name = players.name();
-  //   console.log(name);
-  //   // document.getElementById('#player1').innerHTML = name;  
-  // })
-  
 });
 
-// Get a reference to the root of the chat data.
-  //-   var messagesRef = new Firebase('https://finalprojectcd.firebaseio.com/');
+// Chat process
+// When the user presses enter on the message input, write the message to firebase.
+$('#messageInput1').keypress(function (e) {
+  if (e.keyCode === 13) {
+    var name = $('#chatName1').val();
+    var text = $('#messageInput1').val();
+    chatRef.push({name:name, text:text});
+    $('#messageInput1').val('');
+  }
+});
 
-  //-   // When the user presses enter on the message input, write the message to firebase.
-  //-   $('#messageInput').keypress(function (e) {
-  //-     if (e.keyCode == 13) {
-  //-       var name = $('#nameInput').val();
-  //-       var text = $('#messageInput').val();
-  //-       messagesRef.push({name:name, text:text});
-  //-       $('#messageInput').val('');
-  //-     }
-  //-   });
+$('#messageInput2').keypress(function (e) {
+  if (e.keyCode === 13) {
+    var name = $('#chatName2').val();
+    var text = $('#messageInput2').val();
+    chatRef.push({name:name, text:text});
+    $('#messageInput2').val('');
+  }
+});
 
-  //-   // Add a callback that is triggered for each chat message.
-  //-   messagesRef.limit(10).on('child_added', function (snapshot) {
-  //-     var message = snapshot.val();
-  //-     $('<div/>').text(message.text).prepend($('<em/>')
-  //-       .text(message.name+': ')).appendTo($('#messagesDiv'));
-  //-     $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-  //-   });
+// Add a callback that is triggered for each chat message.
+chatRef.limit(10).on('child_added', function (snapshot) {
+  var message = snapshot.val();
+  $('<div/>').text(message.text).prepend($('<em/>')
+    .text(message.name+': ')).appendTo($('#chatDiv'));
+  $('#chatDiv')[0].scrollTop = $('#chatDiv')[0].scrollHeight;
+});
+
